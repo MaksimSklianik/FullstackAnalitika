7a6a83a9d1650d0e30e7876cfc82492a18750ac9
Maxell <Maksim.sklianik@gmail.com>
2021-12-02 21:17:11 +0200

    generic error handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const keys = require('../config/kyes')
+const errorhender = require('../utilus/errorHandler')
 
 module.exports.login = async function (req, res) {
     const candidate = await User.findOne({
@@ -58,7 +59,7 @@ module.exports.register = async function (req, res) {
             await user.save()
             res.status(201).json(user)
         } catch (e) {
-            //error
+            errorhender(res, e)
         }
 
     }
