77910f7eec8fe62743293c38c57d8d34270a3297
Maxell <Maksim.sklianik@gmail.com>
2021-12-02 21:40:14 +0200

    getting all positions, position functionality

--- controllers/position.js
+++ controllers/position.js
@@ -1,7 +1,57 @@
-module.exports.getByCategoryId = function (req, res) {}
+const Position = require('../models/Position')
+const errorHandler = require('../utilus/errorHandler')
 
-module.exports.create = function (req, res) {}
 
-module.exports.remove = function (req, res) {}
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.CategoryId,
+            user: req.user.id
+        })
+        res.status(200).json(positions)
 
-module.exports.update    = function (req, res) {}
+    } catch (e) {
+        errorHandler(res, e)
+    }
+
+}
+
+module.exports.create = async function (req, res) {
+    try {
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id
+        }).save()
+        res.status(201).json(position)
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
+
+module.exports.remove = async function (req, res) {
+    try {
+        await Position.remove({_id: req.params.id})
+        res.status(200).json({
+            massage: "position remove"
+        })
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
+
+module.exports.update = async function (req, res) {
+    try {
+        const position = await Position.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json(position)
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
