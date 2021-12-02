00fc315d8b96e420103093a9406a5a41569d05bb
Maxell <Maksim.sklianik@gmail.com>
2021-12-02 22:41:29 +0200

    retrieving and deleting categories, configuration, uploading files

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,59 @@
-module.exports.getAll = function (req, res) {
+const Category = require('../models/Category')
+const Position = require("../models/Position")
+const errorHandler = require("../utilus/errorHandler")
+
+
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({user: req.user.id})
+        res.status(200).json(categories)
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 }
-module.exports.getById = function (req, res) {
+module.exports.getById = async function (req, res) {
+    try {
+        const categories = await Category.findById(req.params.id)
+        res.status(200).json(categories)
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 }
-module.exports.getAll = function (req, res) {
-    res.json({massage:'categories'})
+module.exports.getAll = async function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 }
-module.exports.remove = function (req, res) {
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({_id: req.params.id})
+        await Position.remove({category: req.params.id})
+        res.status(200).json({
+            massage:"category deleted"
+        })
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 }
-module.exports.create = function (req, res) {
+module.exports.create = async function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 }
-module.exports.update = function (req, res) {
+module.exports.update = async function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 }
