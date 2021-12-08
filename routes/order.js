1157a04fb9635f48a503984b2b88b70260833fea
Maxell <Maksim.sklianik@gmail.com>
2021-12-08 20:38:01 +0200

    creating categories. uploading pictures to the server

--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const controller = require("../controllers/order")
+const passport = require("passport");
 const router = express.Router()
 
 
-router.post('/', controller.getAll)
+router.post('/',passport.authenticate('jwt', {session: false}), controller.getAll)
 
 
-router.get('/', controller.create)
+router.get('/', passport.authenticate('jwt', {session: false}),controller.create)
+
 module.exports = router
